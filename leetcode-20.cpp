2c52117efc2dd34efc388e03648e8d5665d546e3
Xiaowei7858 <838958635@qq.com>
2020-12-30 23:12:50 +0800

    oj

---
+++ leetcode-20.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+
+        for (char c : s) {
+            if (c == '(')  stack.push(')');
+            else if (c == '[') stack.push(']');
+            else if (c == '{') stack.push('}');
+            else if (stack.empty() || c != stack.top()) return false;
+            else stack.pop();
+        }
+        return stack.empty();
+    }
+};
