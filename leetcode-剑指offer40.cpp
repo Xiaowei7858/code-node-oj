e15bec69656739a40ac0ba157fd812eb6635f324
Xiaowei7858 <838958635@qq.com>
2021-01-13 00:23:41 +0800

    oj

--- leetcode-剑指offer40.cpp
+++ leetcode-剑指offer40.cpp
@@ -1,17 +1,18 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-void AdJustDown(int* array, int k, int root) {
+void AdJustDown(int* a, int n, int root) {
     int parent = root;
     int child = root * 2 + 1;
-    while (child < k) {
-        if (child + 1 < k && array[child + 1] > array[child]) {
-            child++;
+    while (child < n) {
+        if (child + 1 < n && a[child + 1] > a[child]) {
+            ++child;
         }
-        if (array[child] > array[parent]) {
-            int temp = array[parent];
-            array[parent] = array[child];
-            array[child] = temp;
+        if (a[child] > a[parent]) {
+            int temp = a[parent];
+            a[parent] = a[child];
+            a[child] = temp;
+
             parent = child;
             child = parent * 2 + 1;
         }
@@ -22,23 +23,25 @@ void AdJustDown(int* array, int k, int root) {
 }
 int* getLeastNumbers(int* arr, int arrSize, int k, int* returnSize) {
     //建立大堆解决
-    if (!k) return NULL;
-    int* array = (int*)malloc(sizeof(int) * k);
+    *returnSize = k;
+    if (k == 0)
+        return NULL;
+    int* retArr = (int*)malloc(sizeof(int) * k);
+
     for (int i = 0; i < k; ++i) {
-        array[i] = arr[i];
+        retArr[i] = arr[i];
     }
     //建堆
     for (int i = (k - 1 - 1) / 2; i >= 0; --i) {
-        AdJustDown(array, k, i);
+        AdJustDown(retArr, k, i);
     }
 
     //比较
     for (int j = k; j < arrSize; ++j) {
-        if (arr[j] < array[0]) {
-            array[0] = array[j];
-            AdJustDown(array, k, 0);
+        if (arr[j] < retArr[0]) {
+            retArr[0] = arr[j];
+            AdJustDown(retArr, k, 0);
         }
     }
-    *returnSize = k;
-    return array;
+    return retArr;
 }
